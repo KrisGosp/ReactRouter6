8c3902d805c306150ea03f2e8f9e14b923227d30
Kris Gospodinov <kris060601@gmail.com>
2022-05-29 16:40:43 +0300

    Basic React Router 6 usage

---
+++ src/pages/Checkout.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+const Checkout = () => {
+    return ( 
+        <div>
+            <h2>Checkout</h2>
+            <p>Your shopping cart:</p>
+            <p>Pay here</p>
+        </div>
+     );
+}
+ 
+export default Checkout;
